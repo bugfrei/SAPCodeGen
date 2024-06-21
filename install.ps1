14e493d39e8c0285193243185820ef73cf89f8c1
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2024-06-21 23:32:00 +0200

    Installer mit Meldung und direktes laden in aktuelle Powershell Instance

--- install.ps1
+++ install.ps1
@@ -10,6 +10,8 @@ if ($c.Contains("#SAPCodeGen")) {
     return
 }
 $c += "#SAPCodeGen:`nInvoke-RestMethod https://raw.githubusercontent.com/bugfrei/SAPCodeGen/main/supacg.ps1 -OutVariable xxx | out-null; . ([scriptblock]::Create(`$xxx))`n"
-
+Invoke-RestMethod https://raw.githubusercontent.com/bugfrei/SAPCodeGen/main/supacg.ps1 -OutVariable xxx | out-null; . ([scriptblock]::Create($xxx))
 
 Set-Content $PROFILE $c -Force
+
+Write-Host "Installiert und sofort nutzbar." -ForegroundColor Green
