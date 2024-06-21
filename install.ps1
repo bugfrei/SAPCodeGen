c738dddc434607c7a31de8772d027e0a1420fa90
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2024-06-21 23:34:03 +0200

    Installer braucht doch neustart der PS

--- install.ps1
+++ install.ps1
@@ -10,8 +10,7 @@ if ($c.Contains("#SAPCodeGen")) {
     return
 }
 $c += "#SAPCodeGen:`nInvoke-RestMethod https://raw.githubusercontent.com/bugfrei/SAPCodeGen/main/supacg.ps1 -OutVariable xxx | out-null; . ([scriptblock]::Create(`$xxx))`n"
-Invoke-RestMethod https://raw.githubusercontent.com/bugfrei/SAPCodeGen/main/supacg.ps1 -OutVariable xxx | out-null; . ([scriptblock]::Create($xxx))
 
 Set-Content $PROFILE $c -Force
 
-Write-Host "Installiert und sofort nutzbar." -ForegroundColor Green
+Write-Host "Installiert. Neustart der PowerShell notwendig." -ForegroundColor Green
